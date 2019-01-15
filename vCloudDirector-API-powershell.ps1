af6bcb0776e9d78392513979e47cbed4fb18fdd2
MetalJacx <jbenga.iii@gmail.com>
2019-01-15 16:24:50 -0700

    Update vCloudDirector-API-powershell.ps1

--- vCloudDirector-API-powershell.ps1
+++ vCloudDirector-API-powershell.ps1
@@ -3,9 +3,43 @@
 # URI is your Https://<IP or FQDN>;; Leave off api this is due to Vmware now introducing /cloudapi/ when workign with themes
 # APIV the api version you want to use (Current:31.0 as of 1/13/2019)
 
-Function New-vCDLogin($Username,$Password,$Uri,$apiv){
+Function New-vCDLogin{
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateScript({
+            If ($_ -match "^https:\/\/.*"){
+                $True
+            }   
+            else {
+                Throw "$_ Please levearge https:// to the begining or your IP or FQDN."
+            }
+        })]
+        [string]$Uri,
+        [Parameter(Mandatory=$true, Position=1)]
+        [ValidateScript({
+            If ($_ -match "^.*\@.*"){
+                $True
+            }
+            else {
+                Throw "Please make sure to include your Organization (Example <username>@<Org>)."
+            }
+        })]
+        [Alias("user")]
+        [string]$Username,
+        [Parameter(Mandatory=$true, Position=2)]
+        [Alias("pass")]
+        [string]$Password,
+        [Parameter(Mandatory=$true, Position=3)]
+        [ValidateSet("30.0","31.0")]
+        [String]$apiv
+    ) 
+    
+    Write-Host Connecting to "" -ForegroundColor Green -NoNewline; Write-Host "$Uri" "" -NoNewline; Write-Host as "" -ForegroundColor Green -NoNewline; 
+    Write-Host $Username "" -NoNewline; Write-Host with API version "" -ForegroundColor Green -NoNewline; Write-Host $apiv 
+
     $Global:Accept = "application/*+xml;version=$apiv"
     $Global:Uri = $Uri
+    $Global:apiv = $apiv
     $Pair = "$($Username):$($Password)"
     $Bytes = [System.Text.Encoding]::ASCII.GetBytes($Pair)
     $Base64 = [System.Convert]::ToBase64String($Bytes)
@@ -13,17 +47,44 @@ Function New-vCDLogin($Username,$Password,$Uri,$apiv){
     $headers = @{ "Authorization" = $Global:Authorization; "Accept" = $Global:Accept}
     $Res = Invoke-WebRequest -SkipCertificateCheck -Method Post -Headers $headers -Uri "$($Global:Uri)/api/sessions"
     $Global:xvCloudAuthorization = $res.headers["X-VMWARE-VCLOUD-ACCESS-TOKEN"]
-}
+
+
+    Write-Host ...
+    Write-Host Connection Accepted and Session Token Created -ForegroundColor Green
+    Write-Host key: "" -ForegroundColor Y -NoNewline; Write-Host $Global:xvCloudAuthorization
+    }
 
 # Enpoint is the rest of the api command after Https://<IP or FQDN>
 # Example One: -endpoint "api/org"
 # Example Two: -endpoint "cloudapi/branding"
 
-Function Get-vCDRequest($EndPoint){
+Function Get-vCDRequest{
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateScript({
+            If ($_ -match "^api|^cloudapi"){
+                $True
+            }
+            else {
+                Throw "Please make sure to include /api or /cloudapi with your endpoint"
+            }
+        })]
+        [Alias("ep")]
+        [string]$Endpoint
+    )
+
+    If ($Endpoint -match "^cloudapi"){
+        $Global:Bearer = "Bearer $Global:xvCloudAuthorization"
+        $headers = @{"Accept" = "application/json;version=$Global:apiv"; "Authorization" = $Global:Bearer}
+        $Response = Invoke-WebRequest -SkipCertificateCheck -Method Get -Headers $headers -Uri "$($Global:Uri)/$EndPoint"
+        Return $Response
+    }
+    else{
     $Global:Bearer = "Bearer $Global:xvCloudAuthorization"
     $headers = @{"Accept" = $Global:Accept; "Authorization" = $Global:Bearer}
     $Response = Invoke-WebRequest -SkipCertificateCheck -Method Get -Headers $headers -Uri "$($Global:Uri)/$EndPoint"
     Return $Response
+    }
 }
 
 # Body is either XML or JSON file
@@ -45,3 +106,4 @@ Function Post-vCDRequest($EndPoint,$Body,$Type){
     $Response = Invoke-WebRequest -SkipCertificateCheck -Body $Global:body -Method Post -Headers $headers -Uri "$($Global:Uri)/$EndPoint"
     Return $Response
 }
+
